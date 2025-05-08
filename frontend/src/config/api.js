56616fda982d5a6e8fad342e044dc1f198bf420f
JatinPandey8 <jatin812pandey@gmail.com>
2025-05-08 20:19:12 +0530

    spi upgrade

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "https://twitter-db.up.railway.app/"
+export const API_BASE_URL = "https://twitter-db.up.railway.app"
 // export const API_BASE_URL = "http://localhost:5454"
 
 // export const api = axios.create({
