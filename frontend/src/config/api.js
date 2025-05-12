7d6318924647a4830b4ccc31863fde303b2c0299
JatinPandey8 <jatin812pandey@gmail.com>
2025-05-12 11:14:21 +0530

    local

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
-export const API_BASE_URL = "https://twitter-app-2sx2.onrender.com"
-// export const API_BASE_URL = "http://localhost:5454"
+// export const API_BASE_URL = "https://twitter-app-2sx2.onrender.com"
+export const API_BASE_URL = "http://localhost:5454"
 
 // export const api = axios.create({
 //     baseURL: API_BASE_URL, 
